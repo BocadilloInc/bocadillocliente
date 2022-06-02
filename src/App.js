a44cab56a987ede40cb98c355964e25b811d2695
Abdiel Jefte Santillan Luna <54774942+Abdielsantillan@users.noreply.github.com>
2022-06-01 19:08:07 -0500

    nuevo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-
 import firebase , { FirebaseContext } from './firebase';
-
 import Ordenes from './components/paginas/Ordenes';
 import Menu from './components/paginas/Menu';
 import NuevoPlatillo from './components/paginas/NuevoPlatillo';
-
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -16,7 +14,7 @@ function App() {
       }}
     >
           <Routes>
-            <Route path="/" element={<Menu /> } />
+            <Route path="/" element={<Login /> } />
             <Route path="/menu" element={<Menu /> } />
             <Route path="/nuevo-platillo" element={<NuevoPlatillo /> } />
             <Route path="/ordenes" element={<Ordenes /> } />
